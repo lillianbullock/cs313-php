d8e0ec63b0229d8e15aab8f160a86145cd69830c
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-16 13:43:08 -0700

    assignments.php, header, and stylesheet

---
+++ assignments.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang = "en" >
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+            <meta charset = "utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Assignments</title>
+      
+        <!--internal style sheets-->
+        <style> </style>
+
+        <script src=prove01.js> </script>
+          
+    </head>
+    
+    <body>
+        <?php include 'header.html; ?>  
+        <br/>
+      
+        <div>
+            <ul>
+                <li><a href="prove01.html">Homepage</a></li>
+                <li><a href="doesNotExist.html">Shopping Cart</a></li>
+                <li><a href="doesNotExist.html">Big Project 1</a></li>
+            </ul>
+      
+        </div>
+      
+        <footer>© Brooke Bullock, 2019</footer>
+    </body>
+</html>
