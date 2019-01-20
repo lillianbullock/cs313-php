3fc4b66bb1ff278848cfb3d1a60d083091601917
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-19 23:09:55 -0700

    added the images to prove.php

--- web/prove01.php
+++ web/prove01.php
@@ -28,18 +28,28 @@
                 Brobecks was founded by Dallon in 2002, and has been largely
                 inactive since 2009, with an EP released in 2012.</p>
 
+            <img src="https://i.ytimg.com/vi/ieThrn5Ujmg/maxresdefault.jpg"
+                 alt="Still from the Modern Day Cain music Video" style="width:49%"/>
+            <img src="https://pbs.twimg.com/media/DNE00-9X0AI_v5y.jpg"
+                 alt="Dallon and Ryan" style="width:49%; float: right"/>
+            
             <p>The duo's first show, that we know of, was an unanounced show 
                 at Emo Nite on Dec 6, 2016. For the next while they teased 
                 fans about the bands existence, with a few other unanounced
                 shows, as well as cryptic tweets. </p>
             
-            
+            <img src="https://78.media.tumblr.com/13737e0ede85f9178b3bd803a777785a/tumblr_ov9wnuczhU1s8r8kio3_540.jpg"
+                  alt="Ryan playing the drums at a show" style="width:49%"/>
+             <img src="https://78.media.tumblr.com/49662931590a21798442c7e3fe4e240d/tumblr_ov9wnuczhU1s8r8kio1_540.jpg"
+                  alt="Dallon playing the bass and singing at a show" style="width:49%; float: right"/>
             
             <p>The duo officially aknowledged their bands existence mid-2017
                 and headlined their first show July 2017 at the Viper Room.
                 They released two singles before signing to a label. </p>
         </div>
       
+        <div> <a href = "https://www.idkhow.com/">Their official site</a></div>
+        
         <footer>© Brooke Bullock, 2019</footer>
     </body>
 </html>
