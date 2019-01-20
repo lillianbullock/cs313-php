50603afc08bb23626ccbee0cf590a9f1ca42f6cd
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-19 19:23:29 -0700

    paragraphs about IDK written

--- web/prove01.php
+++ web/prove01.php
@@ -20,8 +20,24 @@
         <br/>
       
         <div>
-      
-      
+            <header> My Favorite Band</header>
+    
+            <p>I Don't Know How But They Found Me is a band duo consisting of
+                Dallon Weekes and Ryan Seaman. 
+                They previously were in the band The Brobecks together. The
+                Brobecks was founded by Dallon in 2002, and has been largely
+                inactive since 2009, with an EP released in 2012.</p>
+
+            <p>The duo's first show, that we know of, was an unanounced show 
+                at Emo Nite on Dec 6, 2016. For the next while they teased 
+                fans about the bands existence, with a few other unanounced
+                shows, as well as cryptic tweets. </p>
+            
+            
+            
+            <p>The duo officially aknowledged their bands existence mid-2017
+                and headlined their first show July 2017 at the Viper Room.
+                They released two singles before signing to a label. </p>
         </div>
       
         <footer>© Brooke Bullock, 2019</footer>
