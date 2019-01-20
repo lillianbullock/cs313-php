3526d8303ebd706bf0b03c08f8ff88d8129ae685
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-19 23:13:44 -0700

    fixed the IDKHow images hopefully pt 2

--- web/prove01.php
+++ web/prove01.php
@@ -22,7 +22,7 @@
         <div>
             <header> My Favorite Band</header>
     
-            <p>I Don't Know How But They Found Me is a band duo consisting of
+            <p class = "lightbg">I Don't Know How But They Found Me is a band duo consisting of
                 Dallon Weekes and Ryan Seaman. 
                 They previously were in the band The Brobecks together. The
                 Brobecks was founded by Dallon in 2002, and has been largely
@@ -33,17 +33,17 @@
             <img src="https://pbs.twimg.com/media/DNE00-9X0AI_v5y.jpg"
                  alt="Dallon and Ryan" style="width:40%; float: right"/>
             
-            <p>The duo's first show, that we know of, was an unanounced show 
+            <p class = "lightbg">The duo's first show, that we know of, was an unanounced show 
                 at Emo Nite on Dec 6, 2016. For the next while they teased 
                 fans about the bands existence, with a few other unanounced
                 shows, as well as cryptic tweets. </p>
             
             <img src="https://78.media.tumblr.com/13737e0ede85f9178b3bd803a777785a/tumblr_ov9wnuczhU1s8r8kio3_540.jpg"
-                  alt="Ryan playing the drums at a show" style="width:49%"/>
+                  alt="Ryan playing the drums at a show" style="width:40%"/>
              <img src="https://78.media.tumblr.com/49662931590a21798442c7e3fe4e240d/tumblr_ov9wnuczhU1s8r8kio1_540.jpg"
-                  alt="Dallon playing the bass and singing at a show" style="width:49%; float: right"/>
+                  alt="Dallon playing the bass and singing at a show" style="width:40%; float: right"/>
             
-            <p>The duo officially aknowledged their bands existence mid-2017
+            <p class = "lightbg">The duo officially aknowledged their bands existence mid-2017
                 and headlined their first show July 2017 at the Viper Room.
                 They released two singles before signing to a label. </p>
         </div>
