19fa482d4205ac1142230e611b0eedcbbb734a0c
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-22 17:23:51 -0700

    continents added

--- web/Team03/submit.php
+++ web/Team03/submit.php
@@ -12,12 +12,58 @@
        <form action="submit.php" method="post" >
             Name: <?php echo $_POST["name"]; ?> <br/> <br/>
             
-           Email: <?php echo "mailto:"; echo $_POST["email"]; ?> <br/> <br/>
+            Email: <?php echo "mailto:"; echo $_POST["email"]; ?> <br/> <br/>
                 
             Major: <?php echo $_POST["major"]; ?> <br/> <br/>
               
+            Continents Visited: <br/>
+           
+            <?  if(isset($_POST['NA']) &&
+                $_POST['NA'] == 'Yes')
+                {
+                    echo "North America <br/>";
+                }
+           
+                if(isset($_POST['SA']) &&
+                $_POST['SA'] == 'Yes')
+                {
+                    echo "South America <br/>";
+                }
+           
+                if(isset($_POST['E']) &&
+                $_POST['E'] == 'Yes')
+                {
+                    echo "Europe <br/>";
+                }
+           
+                if(isset($_POST['As']) &&
+                $_POST['As'] == 'Yes')
+                {
+                    echo "Asia <br/>";
+                }
+           
+                if(isset($_POST['Aus']) &&
+                $_POST['Aus'] == 'Yes')
+                {
+                    echo "Australia <br/>";
+                }
+           
+                if(isset($_POST['Af']) &&
+                $_POST['Af'] == 'Yes')
+                {
+                    echo "Africa <br/>";
+                }
+           
+                if(isset($_POST['Ant']) &&
+                $_POST['Ant'] == 'Yes')
+                {
+                    echo "Antarctica <br/>";
+                }
+            
             Comments: <?php echo $_POST["comment"]; ?> <br/> <br/>         
               
+           
+           
             <br/> <br/>
             
           </form>
