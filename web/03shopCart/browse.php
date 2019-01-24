ce67a36f411583f97180101a3e9d6a43d390698d
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-24 11:24:09 -0700

    changed inventory arr

--- web/03shopCart/browse.php
+++ web/03shopCart/browse.php
@@ -13,12 +13,12 @@
     <div>
       
         <?php
-        $_SESSION[$inventory] = array(
+        $_SESSION['inventory'] = array(
             "Mistcloak" => "9.99", 
             "Shardblade" => "234.00", 
             "Shardplate" => "400.00");
        
-        foreach ($_SESSION[$inventory] as $x => $x_val) {
+        foreach ($_SESSION['inventory'] as $x => $x_val) {
             echo $x . " $" . $x_val;
             echo "<form action='add.php?add=" . $x . "' method='post'>";
             echo "<button onclick='submit'>Add to Cart</button>";
