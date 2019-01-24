09f190360e07854b6dcff86717285d9c85e3c915
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-23 21:26:20 -0700

    started shoping card

---
+++ web/03shopCart/browse.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+   <title>About Us</title>
+</head>
+<body>
+   <h1>About Us</h1>
+   <p>
+      
+        Mistcloak 
+        <form action="submit.php" method="post"> 
+            <submit>Add to Cart</submit>    
+    
+        </form>
+       
+       
+    </p>
+</body>
+</html>
