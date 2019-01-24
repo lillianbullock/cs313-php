3e38053800d8df83a470cd73bc01717f33ca979d
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-24 10:42:35 -0700

    bug fix?

--- web/03shopCart/cart.php
+++ web/03shopCart/cart.php
@@ -1,6 +1,5 @@
 <?php session_start(); ?>
 
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,8 +9,6 @@
    <h1>Cart</h1>
    <p>
        <?php
-        $_SESSION[$cart[$_GET[add]]]++;
-       
         foreach ($_SESSION[$cart] as $x => $x_val) {
             echo "<p> " . $x . " [". $x_val . "]";
         }
