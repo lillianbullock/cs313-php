37552dbc5d2b443981a3c315360fc7df911ac0db
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-25 13:05:43 -0700

    changed cart format

--- web/03shopCart/cart.php
+++ web/03shopCart/cart.php
@@ -8,14 +8,34 @@
 <body>
    <h1>Cart</h1>
    <p>
+        <table>
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+                <th>#</th>
+            </tr>
+            <tr>
+                <td>$x</td>
+                <td>$_SESSION['Inventory'][$x]</td>
+                <td>$x_val</td>
+            </tr>
+    
+       
        <?php
         foreach ($_SESSION['cart'] as $x => $x_val) {
-            echo "<p> " . $x . " [". $x_val . "]";
+            echo "<tr>";
+            echo "<td>" . $x . "</td>";
+            echo "<td>" . $_SESSION['Inventory'][$x] . "</td>";
+            echo "<td>" . $x_val . "</td>";
+            echo "</tr>";
         }
        
        //var_dump($_SESSION);
        
        ?>
+            
+        </table>
+    
     </p>
 </body>
 </html>
