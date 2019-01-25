e22dbd26b1cf36a37d55476ccbd2cbe7040d3c89
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-25 13:09:07 -0700

    changed dud header

--- web/03shopCart/cart.php
+++ web/03shopCart/cart.php
@@ -14,12 +14,6 @@
                 <th>Price</th>
                 <th>#</th>
             </tr>
-            <tr>
-                <td>$x</td>
-                <td>$_SESSION['Inventory'][$x]</td>
-                <td>$x_val</td>
-            </tr>
-    
        
        <?php
         foreach ($_SESSION['cart'] as $x => $x_val) {
