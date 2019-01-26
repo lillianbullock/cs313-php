e4c52413b0a37c7cadfb4943fc72dd9d27b54ad4
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-25 21:18:49 -0700

    fixed remove redirect

--- web/03shopCart/remove.php
+++ web/03shopCart/remove.php
@@ -6,7 +6,7 @@ $item = $_POST['rm'];
 if ($_SESSION['cart'][$item] > 0) {
     $_SESSION['cart'][$item]--;
 }
-header("Location: browse.php");
+header("Location: cart.php");
 die();        
        
 ?>
