ad31ef7fa8dfefb70abe16df72483bf5736201cf
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-25 21:36:06 -0700

    formattign adress on confirm

--- web/03shopCart/confirm.php
+++ web/03shopCart/confirm.php
@@ -32,7 +32,8 @@
         <?php
             echo htmlspecialchars($_POST['name']) . "<br/>";
             echo htmlspecialchars($_POST['address']) . "<br/>";
-            if (isset($_POST['address2'])) {
+            
+            if (isset($_POST['address2'] || $_POST['address2'] == "")) {
                 echo htmlspecialchars($_POST['address2']) . "<br/>";
             }
             echo htmlspecialchars($_POST['city']) . " ";
