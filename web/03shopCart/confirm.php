854e6ea32703c194c5528d06f23c397e50836b07
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-25 21:30:42 -0700

    adress fields on confirm page

--- web/03shopCart/confirm.php
+++ web/03shopCart/confirm.php
@@ -15,7 +15,7 @@
                 <th>#</th>
             </tr>
        
-       <?php
+        <?php
         foreach ($_SESSION['cart'] as $x => $x_val) {
             echo "<tr>";
             echo "<td>" . $x . "</td>";
@@ -23,13 +23,21 @@
             echo "<td>" . $x_val . "</td>";
             echo "</tr>";
         }
-            
-            // REMOVE FROM CART BUTTON
-            // SUBTOTAL FIELD
-       ?>
+        ?>
             
         </table>
     
+       
+        <br/><br/>Address:
+        <?php
+            echo htmlspecialchars($_POST['name']) . "<br/>";
+            echo htmlspecialchars($_POST['address']) . "<br/>";
+            echo htmlspecialchars($_POST['address2']) . "<br/>";
+            echo htmlspecialchars($_POST['city']) . " ";
+            echo htmlspecialchars($_POST['state']) . " ";
+            echo htmlspecialchars($_POST['zip']) . "<br/>";
+        ?>
+       
        // ADDRESS INFO 
        
        <br/>
