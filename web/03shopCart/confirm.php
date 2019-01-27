5f2ae928c23f0dcf885e200f8007832f282a5ef9
L. Brooke Bullock <lillianbullock@gmail.com>
2019-01-26 20:06:59 -0700

    cleaned up the formatting of various pages 2

--- web/03shopCart/confirm.php
+++ web/03shopCart/confirm.php
@@ -11,16 +11,16 @@
         <table>
             <tr>
                 <th>Item</th>
-                <th>Price</th>
                 <th>#</th>
+                <th>Price</th>
             </tr>
        
         <?php
         foreach ($_SESSION['cart'] as $x => $x_val) {
             echo "<tr>";
             echo "<td>" . $x . "</td>";
-            echo "<td>" . $_SESSION['inventory'][$x] . "</td>";
             echo "<td>" . $x_val . "</td>";
+            echo "<td>" . $_SESSION['inventory'][$x] . "</td>";
             echo "</tr>";
         }
         ?>
