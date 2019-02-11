bdbd3ef673727b6e7b9795f4004853056b9bb74c
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-11 16:29:32 -0700

    added try-catch around goal insert

--- web/GoalTracker/insert_goal.php
+++ web/GoalTracker/insert_goal.php
@@ -17,45 +17,43 @@
         <?php         
         require 'include/header.php'; 
         require 'include/getDB.php';
-    
+        ?>
+
+        <div class='centre purple'>
+        
+        <?php
         try {
-        // inserts the received goal
-        $stmt = $db->prepare("INSERT INTO goal
-                            ( name, owner, entry_type, frequency_type)
-                            VALUES ( :name
-                            , :owner
-                            , (SELECT common_lookup_id from common_lookup 
-                            where column_name = 'ENTRY_TYPE'
-                            AND   table_name = 'GOAL'
-                            AND  value = :entry_type )
-                            , (SELECT common_lookup_id from common_lookup 
-                            where column_name = 'FREQUENCY_TYPE'
-                            AND   table_name = 'GOAL'
-                            AND  value = :frequency_type ) );");
-        $stmt->execute(array('name' => $_POST['name']
-                            , 'owner' => '1' // TODO get from session when login working
-                            , 'entry_type' => $_POST['entry']
-                            , 'frequency_type' => $_POST['frequency']));
+        // inserts the received goal, and if it fails deals with the
+        // exception instead of just breaking the page
+            $stmt = $db->prepare("INSERT INTO goal
+                                ( name, owner, entry_type, frequency_type)
+                                VALUES ( :name
+                                , :owner
+                                , (SELECT common_lookup_id from common_lookup 
+                                where column_name = 'ENTRY_TYPE'
+                                AND   table_name = 'GOAL'
+                                AND  value = :entry_type )
+                                , (SELECT common_lookup_id from common_lookup 
+                                where column_name = 'FREQUENCY_TYPE'
+                                AND   table_name = 'GOAL'
+                                AND  value = :frequency_type ) );");
+            $stmt->execute(array('name' => $_POST['name']
+                                , 'owner' => '1' // TODO get from session when login working
+                                , 'entry_type' => $_POST['entry']
+                                , 'frequency_type' => $_POST['frequency']));
+           
+            echo "Row inserted";
+
+            $newId = $db->lastInsertId('goal_goal_id_seq');
+            echo "New Id is  ", $newId;
+
         } catch (exception $e)
         {
             echo "The insert failed";
         }
-
-        //$user = $stmt->fetch(); // not needed for insert stmt
-        var_dump($user);
-
-        $newId = $db->lastInsertId('goal_goal_id_seq');
-
-        echo "New Id is  ", $newId;
-
-        // TODO --> how to know if failed other than logs?? - some way to tell user it failed?
         ?>
 
-        <div class='centre purple'>
-            Row inserted
         </div>
-
-
 
         <?php require 'include/footer.php'; ?>
     </body>
