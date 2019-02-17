3d6e779c562b9cad2c7df00a48d5bf8f6275e20f
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-16 18:39:06 -0700

    added session var for user_id

--- web/GoalTracker/delete_g_confirm.php
+++ web/GoalTracker/delete_g_confirm.php
@@ -1,3 +1,5 @@
+session_start();
+
 <!DOCTYPE html>
 <html lang = "en" >
     <head>
@@ -24,10 +26,12 @@
 
 
         <?php
+    var_dump($_SESSION);
+
         $id = $_POST['goal_id'];
         foreach ($db->query("SELECT owner from goal where goal_id = $id;") as $row)
         {
-            if ($row['owner'] != $_SESSION['goal_id']) {// TODO replace when login working 
+            if ($row['owner'] != $_SESSION['user_id']) {// TODO replace when login working 
                 echo "You don't have permission to delete this goal";
             }
             else {
