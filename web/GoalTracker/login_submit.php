37d802d7a8cd6d4ae8db2ecd9b9384e493b3bd33
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-20 20:26:10 -0700

    login

--- web/GoalTracker/login_submit.php
+++ web/GoalTracker/login_submit.php
@@ -1,21 +1,49 @@
-<?php
+<?php session_start(); ?>
 
-    $stmt = $db->prepare('SELECT password, person_id, username
-                        FROM person
-                        WHERE email = :email;');
-    $stmt->execute(array(':email' => $_POST['email'])); 
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+<!DOCTYPE html>
+<html lang = "en" >
+    <head>
+        <link rel="stylesheet" type="text/css" href="include/style.css">
+            <meta charset = "utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+      
+        <!--internal style sheets-->
+        <style> </style>
 
-    if (password_verify($rows[0]['password'], $_POST['password'])) {
+        <!-- <script src=prove01.js> </script> -->
+                  
+    </head>
+    
+    <body>
+        <?php         
+        require 'include/header.php'; 
+        require 'include/getDB.php';
+        ?>
+
+        <div class='centre purple'>
+        
+        <?php
+        $stmt = $db->prepare('SELECT password, person_id, username
+                FROM person
+                WHERE email = :email;');
+        $stmt->execute(array(':email' => $_POST['email'])); 
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if (password_verify($rows[0]['password'], $_POST['password'])) {
         $_SESSION['user_id'] = $rows['person_id'];
         $_SESSION['name'] = $rows['username'];
         echo "login successful. Welcome ";
         echo $_SESSION['name'];
-    } else {
+        } else {
         echo 'login failed';
-    }
+        }
 
-    $_SESSION['user_id'] = 1;
+        $_SESSION['user_id'] = 1;
+        ?>
 
+        </div>
 
-?>
+        <?php require 'include/footer.php'; ?>
+    </body>
+</html>
