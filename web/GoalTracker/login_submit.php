51e4d10c9c65bacc76360a0ff48016c9694f7336
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-20 20:21:52 -0700

    login

--- web/GoalTracker/login_submit.php
+++ web/GoalTracker/login_submit.php
@@ -9,7 +9,8 @@
     if (password_verify($rows[0]['password'], $_POST['password'])) {
         $_SESSION['user_id'] = $rows['person_id'];
         $_SESSION['name'] = $rows['username'];
-        echo "login successful. Welcome $_SESSION['name']";
+        echo "login successful. Welcome ";
+        echo $_SESSION['name'];
     } else {
         echo 'login failed';
     }
