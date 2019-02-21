a067f4ae20080600117121a346c268435199a29c
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-20 19:33:14 -0700

    create_user

--- web/GoalTracker/insert_user.php
+++ web/GoalTracker/insert_user.php
@@ -46,7 +46,7 @@
                 //echo "New Id is  ", $newId;
 
             } catch (exception $e) {
-                echo "The insert failed";
+                echo "The insert failed $e";
             }
         }
         ?>
