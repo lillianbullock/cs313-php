65ee0981b8046807b4d9ae7f035eb00b6cec7f42
L. Brooke Bullock <lillianbullock@gmail.com>
2019-02-22 23:53:54 -0700

    cleaning up things

--- web/GoalTracker/update_entry.php
+++ web/GoalTracker/update_entry.php
@@ -44,15 +44,6 @@
         }
         ?>
 
-            <form action="/GoalTracker/goal_view.php" method="POST">
-                <?php
-                echo '<input type="hidden" name="goal_id" value="';
-                echo $_POST['goal_id'];
-                echo '">';
-                ?>
-                <input type="submit" value="Return to Goal">
-            </form> 
-
         <?php require 'include/footer.php'; ?>
     </body>
 </html>
